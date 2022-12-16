06c79a169daca5a632057bd2ae06e224378d383f
jeremdh62 <jdhaille@myges.fr>
2022-12-16 17:48:43 +0100

    add field and formik

---
+++ src/components/Formik.vue
@@ -0,0 +1,20 @@
+<script setup>
+    defineProps({
+        initialValues: {
+            type: Object,
+            required: true
+        },
+        validate: {
+            type: Function,
+            required: true
+        }
+    })
+
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+</style>
